5c1c11e2cbf461f5d7afc11223191a0aeedcd641
Ammar Ben Khadra <ammarbenkhadra@gmail.com>
2017-05-03 15:24:20 +0200

    Initial commit

---
+++ src/SynthLib2ParserTester.cpp
@@ -0,0 +1,32 @@
+//===------------------------------------------------------------*- C++ -*-===//
+//
+// This file is distributed under BSD License. See LICENSE.txt for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// Copyright (c) 2017,
+// Ammar Ben Khadra,
+// University of Kaiserslautern
+//
+// Copyright (c) 2013,
+// Abhishek Udupa, Mukund Raghothaman,
+// The University of Pennsylvania
+//
+//===----------------------------------------------------------------------===//
+//
+
+#include <SynthLib2ParserIFace.hpp>
+
+using namespace SynthLib2Parser;
+
+int main(int argc, char* argv[])
+{
+    SynthLib2Parser::SynthLib2Parser Parser;
+    try {
+        Parser(argv[1]);
+    } catch (const exception& Ex) {
+        cout << Ex.what() << endl;
+        exit(1);
+    }
+    cout << *(Parser.GetProgram()) << endl;
+}
